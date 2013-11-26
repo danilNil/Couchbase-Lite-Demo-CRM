bc07a15939706134bb31b84d6bdf1033aa7a9975
Danil Nikiforov <danil.nikiphorov@gmail.com>
2013-11-26 12:51:27 +0700

    added new models for contact and sales person

---
+++ CBLiteCRM/CBLiteCRM/Classes/Data/Models/Contact.m
@@ -0,0 +1,13 @@
+//
+//  Contact.m
+//  CBLiteCRM
+//
+//  Created by Danil on 26/11/13.
+//  Copyright (c) 2013 Danil. All rights reserved.
+//
+
+#import "Contact.h"
+
+@implementation Contact
+
+@end
---
+++ CBLiteCRM/CBLiteCRM/Classes/Data/Models/SalesPerson.h
@@ -0,0 +1,13 @@
+//
+//  SalesPerson.h
+//  CBLiteCRM
+//
+//  Created by Danil on 26/11/13.
+//  Copyright (c) 2013 Danil. All rights reserved.
+//
+
+#import <CouchbaseLite/CouchbaseLite.h>
+
+@interface SalesPerson : CBLModel
+
+@end
