3bab74c44809426c3c65aaf86d7dd6f6cbff9c33
Danil Nikiforov <danil.nikiphorov@gmail.com>
2013-11-26 08:53:07 +0700

    added Xcode project

---
+++ CBLiteCRM/CBLiteCRM/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CBLiteCRM
+//
+//  Created by Danil on 26/11/13.
+//  Copyright (c) 2013 Danil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
