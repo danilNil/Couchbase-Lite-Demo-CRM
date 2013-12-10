ecd913c6ac13f5a64e1b65622ac777e99869265a
danilNil <danil.nikiphorov@gmail.com>
2013-12-11 00:53:02 +0700

    updated sync function from Skype conversation

--- CBLiteCRM/sync_function.js
+++ CBLiteCRM/sync_function.js
@@ -6,3 +6,28 @@ function(doc) {
   }
   channel(doc.channels);
 }
+
+
+// sync function we created with Chris
+
+function(doc, oldDoc) {
+  channel("all_docs");
+  if (doc.type == "salesperson") {
+    var userID = doc.user_id;
+    if (userID == "dur-perturbador@yandex.ru") {
+      role(userID, "role:admin")
+    }
+    if (oldDoc) {
+      // only admin can change existing salespeople
+      requireRole("admin")
+      if (doc.approved) {
+        // only sales people who have been approved can sync
+        access(userID, "all_docs")
+      }
+    }
+    else {
+      // salespeople can only be created by themselves need to uncomment when we removed fake customers creation
+      //      requireUser(userID)
+    }
+  }
+}
