99af65ea3c34a55bf44f58b19565e79b65a3a81f
Andrew Tokarev <andrew.tokarev@aldigit.com>
2013-12-12 21:14:07 +0700

    Contact+Helper category added for common tasks that should not be in views

---
+++ CBLiteCRM/CBLiteCRM/Classes/Data/Models/Contact+Helper.h
@@ -0,0 +1,17 @@
+//
+//  Contact+Helper.h
+//  CBLiteCRM
+//
+//  Created by Andrew on 12.12.13.
+//  Copyright (c) 2013 Danil. All rights reserved.
+//
+
+#import "Contact.h"
+
+@interface Contact (Helper)
+
+- (NSString*)customerCompanyName;
+
+- (NSString*)positionAtCompany;
+
+@end
---
+++ CBLiteCRM/CBLiteCRM/Classes/Data/Models/Contact+Helper.m
@@ -0,0 +1,42 @@
+//
+//  Contact+Helper.m
+//  CBLiteCRM
+//
+//  Created by Andrew on 12.12.13.
+//  Copyright (c) 2013 Danil. All rights reserved.
+//
+
+#import "Contact+Helper.h"
+#import "Customer.h"
+
+@implementation Contact (Helper)
+
+- (NSString*)customerCompanyName
+{
+    return self.customer.companyName;
+}
+
+- (NSString*)positionAtCompany
+{
+    NSMutableString * positionAtCompany = [NSMutableString new];
+    
+    if ([self hasPosition])
+        [positionAtCompany appendFormat:@"%@ ", self.position];
+    
+    if ([self hasCompanyName])
+        [positionAtCompany appendFormat:@"at %@", [self customerCompanyName]];
+    
+    return positionAtCompany;
+}
+
+- (BOOL)hasPosition
+{
+    return [self.position length] != 0;
+}
+
+- (BOOL)hasCompanyName
+{
+    return [[self customerCompanyName] length] != 0;
+}
+
+@end
