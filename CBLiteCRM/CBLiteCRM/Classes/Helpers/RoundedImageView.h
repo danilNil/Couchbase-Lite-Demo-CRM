f166423c42bf62e0810ae63423d5b83f132894e8
Andrew Tokarev <andrew.tokarev@aldigit.com>
2013-12-12 21:15:22 +0700

    RoundedImageView added with Corner Radius == 5 and BorderWidth == 0.5, there is also kBaseGrayColor is used.

---
+++ CBLiteCRM/CBLiteCRM/Classes/Helpers/RoundedImageView.h
@@ -0,0 +1,15 @@
+//
+//  RoundedImageView.h
+//  CBLiteCRM
+//
+//  Created by Andrew on 12.12.13.
+//  Copyright (c) 2013 Danil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoundedImageView : UIImageView
+
+- (void)setCornerRadius:(CGFloat)cornerRadius borderWidth:(CGFloat)borderWidth;
+
+@end
---
+++ CBLiteCRM/CBLiteCRM/Classes/Helpers/RoundedImageView.m
@@ -0,0 +1,46 @@
+//
+//  RoundedImageView.m
+//  CBLiteCRM
+//
+//  Created by Andrew on 12.12.13.
+//  Copyright (c) 2013 Danil. All rights reserved.
+//
+
+#import "RoundedImageView.h"
+
+#import <QuartzCore/QuartzCore.h>
+
+#define kRoundedImageViewDefaultCorderRadius 5.f
+#define kRoundedImageViewDefaultBorderWidth  0.5
+
+@implementation RoundedImageView
+
+- (void)setCornerRadius:(CGFloat)cornerRadius borderWidth:(CGFloat)borderWidth
+{
+    self.layer.masksToBounds = YES;
+    self.layer.cornerRadius  = cornerRadius;
+    self.layer.borderWidth   = borderWidth;
+    self.layer.borderColor   = kBaseGrayColor.CGColor;
+    
+    self.layer.shouldRasterize = YES;
+    self.layer.rasterizationScale = [UIScreen mainScreen].scale;
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if  (self) {
+        [self setCornerRadius:kRoundedImageViewDefaultCorderRadius
+                  borderWidth:kRoundedImageViewDefaultBorderWidth];
+    }
+    return self;
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    
+    [self setCornerRadius:kRoundedImageViewDefaultCorderRadius
+              borderWidth:kRoundedImageViewDefaultBorderWidth];
+}
+
+@end
