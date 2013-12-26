19b22db1970c3f02f78223b4d80380514fc9364b
Danil Nikiforov <danil.nikiphorov@gmail.com>
2013-12-26 12:12:52 +0700

    added new project instead

---
+++ SyncManagetTestApp/SyncManagetTestApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SyncManagetTestApp
+//
+//  Created by Danil on 26/12/13.
+//  Copyright (c) 2013 Danil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SyncManagetTestApp/SyncManagetTestApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SyncManagetTestApp
+//
+//  Created by Danil on 26/12/13.
+//  Copyright (c) 2013 Danil. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
