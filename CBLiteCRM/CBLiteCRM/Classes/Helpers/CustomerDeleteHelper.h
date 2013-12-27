aab5474f9140e607de6a4395eaa132256131e269
Ruslan Musagitov <ruslan.musagitov@aldigit.com>
2013-12-27 14:46:30 +0700

    .h file added

---
+++ CBLiteCRM/CBLiteCRM/Classes/Helpers/CustomerDeleteHelper.h
@@ -0,0 +1,13 @@
+//
+//  CustomerDeleteHelper.h
+//  CBLiteCRM
+//
+//  Created by Ruslan on 12/27/13.
+//  Copyright (c) 2013 Couchbase. All rights reserved.
+//
+
+#import "CBLModelDeleteHelper.h"
+
+@interface CustomerDeleteHelper : CBLModelDeleteHelper
+
+@end
