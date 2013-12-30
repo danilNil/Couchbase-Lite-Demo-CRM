d5e03afa8be2f4520e588b8bfe4b7816bb91b6d8
Ruslan Musagitov <ruslan.musagitov@aldigit.com>
2013-12-30 16:41:12 +0700

    alert changed

--- CBLiteCRM/CBLiteCRM/Classes/Helpers/CustomerDeleteHelper.m
+++ CBLiteCRM/CBLiteCRM/Classes/Helpers/CustomerDeleteHelper.m
@@ -14,7 +14,6 @@
 @interface CustomerDeleteHelper ()
 {
     UIAlertView* deletionAlertView;
-    UIAlertView* relationalImpactAlertView;
     ContactsStore* contactsStore;
     OpportunitiesStore* opportunitiesStore;
     CBLQuery* relatedContactsQuery;
@@ -44,34 +43,45 @@
     return self;
 }
 
-- (void)initialize {
+- (void)initialize
+{
     contactsStore = [DataStore sharedInstance].contactsStore;
     opportunitiesStore = [DataStore sharedInstance].opportunitiesStore;
     deletionAlertView = [[UIAlertView alloc] initWithTitle:@"Delete item" message:@"Are you sure you want to remove item" delegate:self cancelButtonTitle:@"NO" otherButtonTitles:@"YES", nil];
-    relationalImpactAlertView = [[UIAlertView alloc] initWithTitle:@"Warning" message:@"" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil];
 }
 
 - (void)setItem:(CBLModel *)item
 {
     _item = item;
+    [self setDeleteMessageForItem:item];
+}
 
+- (void)setDeleteMessageForItem:(CBLModel *)item
+{
     NSError* error;
     NSMutableString* msg = [NSMutableString new];
-
     relatedContactsQuery = [contactsStore queryContactsForCustomer:(Customer*)item];
     NSUInteger ctCount = [[relatedContactsQuery run:&error] count];
-    if (ctCount > 0)
-        [msg appendFormat:@"%u contacts", ctCount];
 
     relatedOpportunitiesQuery = [opportunitiesStore queryOpportunitiesForCustomer:(Customer*)item];
     NSUInteger oppCount = [[relatedOpportunitiesQuery run:&error] count];
 
+    if (ctCount == 1)
+        [msg appendFormat:@"%u contact", ctCount];
+    else if (ctCount > 1)
+        [msg appendFormat:@"%u contacts", ctCount];
+
     if (ctCount > 0 && oppCount > 0)
         [msg appendString:@" and "];
-    if (oppCount > 0)
+    if (oppCount == 1)
+        [msg appendFormat:@"%u opportunity", oppCount];
+    else if (oppCount > 1)
         [msg appendFormat:@"%u opportunities", oppCount];
 
-    relationalImpactAlertView.message = [NSString stringWithFormat:@"%@ will be deleted", msg];
+    if (oppCount + ctCount > 0)
+        deletionAlertView.message = [NSString stringWithFormat:@"Are you sure you want to remove item.\nAlso %@ will be deleted", msg];
+    else
+        deletionAlertView.message = @"Are you sure you want to remove item";
 }
 
 - (void)showDeletionAlert
@@ -101,9 +111,7 @@
 
 -(void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
 {
-    if (alertView == deletionAlertView && buttonIndex == 1) {
-        [relationalImpactAlertView show];
-    } else if (alertView == relationalImpactAlertView && buttonIndex == 1) {
+    if (buttonIndex == 1) {
         NSError *error;
         [self deleteRelatedObjects];
         [self.item deleteDocument:&error];
