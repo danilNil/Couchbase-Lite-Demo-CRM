f29005899033f0dc282a7e51f301ae54177b1c09
Danil Nikiforov <danil.nikiphorov@gmail.com>
2014-01-18 15:53:05 +0700

    update sync function

--- CBLiteCRM/sync_function.js
+++ CBLiteCRM/sync_function.js
@@ -1,4 +1,4 @@
-
+ 
 // grant access to all users
 function(doc) {
   channel("all_channels");
@@ -10,26 +10,32 @@ function(doc) {
 }
 
 
-// sync function we created with Chris
+// final sync function
 
 function(doc, oldDoc) {
   channel("all_docs");
-  if (doc.type == "salesperson") {
+  if (doc.type === "salesperson") {
     var userID = doc.user_id;
-    if (userID == "dur-perturbador@yandex.ru") {
+    //if user logged as admin we add him access to all_docs channel and role
+    if (doc.isAdmin) {
+      access(userID, "all_docs")
       role(userID, "role:admin")
     }
     if (oldDoc) {
-      // only admin can change existing salespeople
-      requireRole("admin")
-      if (doc.approved) {
-        // only sales people who have been approved can sync
+      //salespeople can only be approved by admin. when we approve sales person we grant him access to all_docs channel
+      if(doc.approved && !oldDoc.approved){
         access(userID, "all_docs")
+        requireRole("admin")
+      }
+      else{
+        //salespeople can only be edited by themselves
+        requireUser(userID)  
       }
     }
-    else {
-      // salespeople can only be created by themselves need to uncomment when we removed fake customers creation
-      //      requireUser(userID)
+    else{
+      //salespeople can only be created by themselves
+      requireUser(userID)
     }
   }
 }
+
